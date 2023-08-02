cfadc671dccddd511c91a43ca117cc6c5ede0305
Gibeom Lim <delphox60@gmail.com>
2023-08-02 19:21:33 +0900

    feat: implement playing game by prompting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
+from random import choice
+from os import system
+from time import sleep
 
 
 def match_manitto(participants):
@@ -7,7 +9,7 @@ def match_manitto(participants):
 
     for participant in participants:
         while True:
-            manitto = random.choice(remain_members)
+            manitto = choice(remain_members)
             if manitto != participant:
                 break
         manitto_matchings[participant] = manitto
@@ -15,5 +17,24 @@ def match_manitto(participants):
     return manitto_matchings
 
 
-participants_file = open("participants.txt", "r")
-participants = participants_file.read().split("\n")
+def get_participants():
+    participants_file = open("participants.txt", "r")
+    return participants_file.read().split("\n")
+
+
+if __name__ == "__main__":
+    participants = get_participants()
+    manitto_matchings = match_manitto(participants)
+
+    while True:
+        name = input("이름을 입력하세요: ")
+
+        if name not in manitto_matchings.keys():
+            print("참가자 명단에서 이름을 찾을 수 없습니다. 올바른 이름을 다시 입력해 주세요.")
+            continue
+
+        print(name + "님의 마니또는 " + manitto_matchings[name] + "입니다.")
+        print("3초 뒤에 문구가 사라집니다.")
+
+        sleep(3)
+        system("clear")
