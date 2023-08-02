d0c09d24815b471fb3b547c68d7784dd2e7287c4
Gibeom Lim <delphox60@gmail.com>
2023-08-02 19:09:21 +0900

    feat: implement function matches manitto

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import random
+
+
+def match_manitto(participants):
+    manitto_matchings = {}
+    remain_members = participants
+
+    for participant in participants:
+        while True:
+            manitto = random.choice(remain_members)
+            if manitto != participant:
+                break
+        manitto_matchings[participant] = manitto
+
+    return manitto_matchings
+
+
 participants_file = open("participants.txt", "r")
 participants = participants_file.read().split("\n")
